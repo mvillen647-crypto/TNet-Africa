cd0e2013f63fc9c2f9ed5617a1da3e624afc4372
mvillen647-crypto <mvillen647@gmail.com>
2026-06-26 22:15:27 +0300

    Create feed.js

---
+++ features/feed/feed.js
@@ -0,0 +1,13 @@
+export function renderFeed(app) {
+  app.innerHTML = `
+    <div class="page">
+
+      <h1>📰 Feed</h1>
+
+      <div class="card">
+        <p>No scans yet</p>
+      </div>
+
+    </div>
+  `;
+}
